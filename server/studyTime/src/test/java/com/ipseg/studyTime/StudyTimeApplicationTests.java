e99e1522cedd6344e4cc90439a53ac6bbe58e351
xzcv1994 <32008123+xzcv1994@users.noreply.github.com>
2022-12-03 23:08:11 +0900

    init
    init

---
+++ server/studyTime/src/test/java/com/ipseg/studyTime/StudyTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipseg.studyTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
