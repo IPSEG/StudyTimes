413c64996478961741943f7985b009a8010c7bda
xzcv1994 <32008123+xzcv1994@users.noreply.github.com>
2023-01-01 17:40:57 +0900

    Delete server directory
    gradle 재설정

--- server/studyTime/src/main/java/com/ipseg/studyTime/framework/initializer/ServletInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.ipseg.studyTime.framework.initializer;
-
-import com.kt.aicare.AicareApiApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-}
