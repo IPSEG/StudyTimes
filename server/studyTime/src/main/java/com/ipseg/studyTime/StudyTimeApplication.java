dffe7b111e9cd4594ba2f7ff3e596d583acbaea3
xzcv1994 <32008123+xzcv1994@users.noreply.github.com>
2023-01-01 18:18:20 +0900

    backend_master_01
    commit01 : 기본 디렉터리 구조 설정

---
+++ server/studyTime/src/main/java/com/ipseg/studyTime/StudyTimeApplication.java
@@ -0,0 +1,13 @@
+package com.ipseg.studyTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTimeApplication.class, args);
+	}
+
+}
---
+++ server/studyTime/src/main/java/com/ipseg/studyTime/framework/config/WebMvcConfig.java
@@ -0,0 +1,8 @@
+package com.ipseg.studyTime.framework.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+}
