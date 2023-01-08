6a0276828133050969621c184560329fef0476fc
양승빈 (AI IoT플랫폼개발TF) <seungbin.yang@kt.com>
2023-01-08 21:28:10 +0900

    filter 기본 설정 추가, spring security dependency 추가, application 파일 생성

---
+++ server/studyTime/src/main/java/com/ipseg/studyTime/framework/config/SpringSecurityConfig.java
@@ -0,0 +1,9 @@
+package com.ipseg.studyTime.framework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+}
--- server/studyTime/src/main/java/com/ipseg/studyTime/framework/config/WebMvcConfig.java
+++ server/studyTime/src/main/java/com/ipseg/studyTime/framework/config/WebMvcConfig.java
@@ -1,8 +1,16 @@
 package com.ipseg.studyTime.framework.config;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebMvcConfig implements WebMvcConfigurer {
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setOrder(1);
 
+        return filterRegistrationBean;
+    }
 }
