c5bcbbc615a5425d59d78888f552fe2c5359b620
xzcv1994 <32008123+xzcv1994@users.noreply.github.com>
2023-02-17 23:29:14 +0900

    Backend develop (#9)
    * Timer - 01
    
    timer 프로토타입 개발 mapper 및 db 설정 파일 추가
    
    * Timer - 02
    
    timer
    - timer add/get 기능 초기 구현
    
    ---------
    
    Co-authored-by: 양승빈 (AI IoT플랫폼개발TF) <seungbin.yang@kt.com>

---
+++ server/studyTime/src/main/java/com/ipseg/studyTime/framework/config/DataSourceConfig.java
@@ -0,0 +1,23 @@
+package com.ipseg.studyTime.framework.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    public DataSource dataSource(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("org.postgresql.Driver");
+        hikariConfig.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/studytimes");
+        hikariConfig.setUsername("postgres");
+        hikariConfig.setPassword("vhtmrmfptm1!");
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
+        return dataSource;
+    }
+}
